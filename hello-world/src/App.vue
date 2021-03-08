06d4a819ba07d216fb7960c60e58d90e1317b986
Saicharan67 <mahankalisaicharan@gmail.com>
2021-03-08 22:42:51 +0530

    initialized project and created hello world

---
+++ hello-world/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+ <p>Hello {{name}}</p>
+ <p v-text="college"> </p> <!--v-bind-->
+ <p v-html="htmlbinding"></p><!--v-text-->
+ <h2 v-bind:id="headingID">Heading</h2>
+ <h2 v-bind:class="status">Status</h2>
+ <button v-bind:disabled="isDisabled">button</button>
+  <h2 v-bind:class="isPromoted && 'promoted'">promoted</h2>
+  <h2 v-bind:class="isSold?'sold':'new'"> Soldout? movie</h2>
+  <h2 v-bind:class="['new','promoted']">new promoted movie</h2>
+   <h2 v-bind:class="[isPromoted && 'promoted',isSold?'sold':'new']">array conditionality</h2>
+  <h2 v-bind:class="{
+    promoted: isPromoted,
+    new:!isSold,
+    'sold':isSold
+    }">object conditionality</h2>
+ </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      name:'Sai Charan',
+      college:"NIT AP",
+      htmlbinding:"<b>ECE</b>",
+      headindID:'heading',
+      isDisabled:true,
+      status:'danger',
+      isPromoted:true,
+      isSold:true
+    };
+  },
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.underline{
+  text-decoration: underline;
+}
+.promoted
+{
+  font-style: italic;
+}
+.new{
+  color: green;
+}
+.sold{
+  color:red;
+}
+</style>
