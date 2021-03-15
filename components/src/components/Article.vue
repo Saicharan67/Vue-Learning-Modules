3d121e0f79beb7fedaef889b3cb023b7bce24053
Saicharan67 <mahankalisaicharan@gmail.com>
2021-03-15 20:44:29 +0530

    added components

---
+++ components/src/components/Article.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2 v-bind="$attrs">{{ title }}</h2>
+    <h2>Likes - {{ likes }}</h2>
+    <h2>Published - {{ isPublished ? 'Yes' : 'No' }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: 'Article default title',
+    },
+    likes: Number,
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  // props:['title','likes','isPublished'],
+  inheritAttrs: false,
+}
+</script>
+
+<style scoped>
+</style>
