c8ad3ddb9d25efdb1e3d3d7cf7085db2d4134110
Saicharan67 <mahankalisaicharan@gmail.com>
2021-04-15 20:33:22 +0530

    added compositions

---
+++ compositions/src/components/Data.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div>
+        <h2>Options - {{o_firstName}}</h2>
+        <h2>Compositions - {{c_firstName}}</h2>
+    </div>
+</template>
+<script>
+import {ref} from 'vue'
+export default {
+    name: 'Data',
+    setup() {
+        const c_firstName = ref('Charan')
+        console.log({c_firstName})
+        return{
+            c_firstName
+        }
+    },
+    data(){
+        return{
+            o_firstName:"Sai"
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
