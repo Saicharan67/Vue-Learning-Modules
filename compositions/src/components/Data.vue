8934ed7272a1bd76099d4211bd94ead8d0ed353a
Saicharan67 <mahankalisaicharan@gmail.com>
2021-04-16 20:10:03 +0530

    Replacing data with reactive

--- compositions/src/components/Data.vue
+++ compositions/src/components/Data.vue
@@ -1,27 +1,45 @@
 <template lang="">
-    <div>
-        <h2>Options - {{o_firstName}}</h2>
-        <h2>Compositions - {{c_firstName}}</h2>
-    </div>
+  <div>
+    <h2>Options - {{ o_firstName }}</h2>
+    <h2>Options - {{ o_firstName }} {{o_LastName}} a k a {{o_heroName}} </h2>
+    <h2>Compositions - {{ c_firstName }}</h2>
+    <h2>Compositions - {{ c_firstName }} {{c_lastName}} a k a {{c_heroName}} </h2>
+    <h2>Reactive Compositions - {{state.firstName}} {{state.lastName}} a k a {{state.heroName}}
+  </div>
 </template>
 <script>
-import {ref} from 'vue'
+import { ref , reactive} from "vue";
 export default {
-    name: 'Data',
-    setup() {
-        const c_firstName = ref('Charan')
-        console.log({c_firstName})
-        return{
-            c_firstName
-        }
-    },
-    data(){
-        return{
-            o_firstName:"Sai"
-        }
-    }
-}
-</script>
-<style lang="">
+  name: "Data",
+  setup() {
     
-</style>
+    //ref returns  a reactive and immutable  reference to the internal value its holding and we can use it by .value
+    const c_firstName = ref("Clark");
+    const c_lastName = ref("kent")
+    const c_heroName = ref("SuperMan")
+    console.log({ c_firstName });
+
+    const state = reactive({
+      firstName: "Princess",
+      lastName: "Diana",
+      heroName: "Wonder Women"
+    });
+
+    return {
+      c_firstName,
+      c_lastName,
+      c_heroName,
+      state
+    };
+  },
+  data() {
+
+    return {
+      o_firstName: "Bruce",
+      o_LastName: "Wayne",
+      o_heroName:"BatMan"
+    };
+  },
+};
+</script>
+<style lang=""></style>
