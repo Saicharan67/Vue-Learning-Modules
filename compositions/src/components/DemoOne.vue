b9593d579e4c3a0a3c1702e5cebf4cc4d4a09b2f
Saicharan67 <mahankalisaicharan@gmail.com>
2021-04-17 19:48:55 +0530

    added reactive torefs usage

---
+++ compositions/src/components/DemoOne.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div>
+        <h2> {{name}} </h2>
+    </div>
+</template>
+<script>
+import {ref } from "vue"
+export default {
+    name:"DemoOne",
+    setup(){
+        //let name1 = "mahankali"
+        //By returing a const or let value vue doesnt know how to react to change in values so we use reactive or refs for that
+        let name = ref("Charan")
+        setTimeout(() => {
+            name.value = "Sai"
+            //name1 = "xyz"
+        }, 2000);
+        return{
+            //name1
+            name
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ compositions/src/components/DemoTwo.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        <h2> {{firstName}} {{lastName}} </h2>
+    </div>
+</template>
+<script>
+import {reactive,toRefs} from "vue"
+export default {
+    name:"DemoTwo",
+    setup()
+    {
+        const state = reactive({
+            firstName:"Sai",
+            lastName:"Charan"
+        })
+        setTimeout(() => {
+            state.firstName = "Xyz"
+            state.lastName = "Abc"
+        }, 2000);
+        //By returning the values like below vue cant react to changes on them so we should toRefs method to solve this
+        // return{
+        //     firstName :state.firstName,
+        //     lastName:state.lastName
+        //}
+        return toRefs(state)
+    }
+}
+</script>
+<style lang="">
+    
+</style>
