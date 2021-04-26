13ec3229112aef33fae8611f6e7c105c05b0d988
Saicharan67 <mahankalisaicharan@gmail.com>
2021-04-26 21:08:38 +0530

    added watchers with reactive function

--- compositions/src/components/Watch.vue
+++ compositions/src/components/Watch.vue
@@ -2,21 +2,35 @@
     <div>
         <input type='text' placeholder="Name" v-model="name"/>
         <input type='text' placeholder="Name" v-model="firstName"/>
+        <input type='text' placeholder="reactiveName" v-model="fname"/>
+  
     </div>
 </template>
 <script>
-import { ref ,watch} from 'vue'
+import { ref ,watch,reactive,toRefs} from 'vue'
 
 export default {
     name:'Watch',
     setup() {
       const firstName = ref('') 
+      const state = reactive({
+          fname:'',
+
+      })
       watch(firstName,(newValue,oldValue)=>{
            console.log({oldValue})
             console.log({newValue})
       })
+      watch(()=>{
+          return {...state}
+      },(newValue,oldValue)=>{
+          console.log(oldValue.fname)
+          console.log(newValue.fname)
+      })
       return{
-          firstName
+          firstName,
+            ...toRefs(state)
+
       } 
       
     },
@@ -24,6 +38,7 @@ export default {
     {
         return{
             name:'',
+          
         }
     },
     watch:{
