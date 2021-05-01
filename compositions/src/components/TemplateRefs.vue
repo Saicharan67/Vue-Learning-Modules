f2888dece67c455153805ece80c7eaca1f5f180b
Saicharan67 <mahankalisaicharan@gmail.com>
2021-05-01 20:24:22 +0530

    Added Template Refs

---
+++ compositions/src/components/TemplateRefs.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div>
+        <input type="text" ref="inputRef"/>
+    </div>
+</template>
+<script>
+import {onMounted, ref} from 'vue'
+export default {
+    name:"templateRef",
+    setup() {
+        const inputRef = ref(null)
+        onMounted(()=>{
+            inputRef.value.focus()
+        })
+           return{
+        inputRef
+    }
+    },
+ 
+}
+</script>
+<style lang="">
+    
+</style>
