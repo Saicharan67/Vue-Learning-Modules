a2901e57046a80f8f9264ca8750b754b6c1dd9fa
Saicharan67 <mahankalisaicharan@gmail.com>
2021-05-02 20:38:56 +0530

    added composables

---
+++ compositions/src/components/ClickCounter.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div>
+        <button @click="incrementCount">Clicked {{count}} times</button>
+    </div>
+</template>
+<script>
+//import counter from '../mixins/counter'
+import useCounter from '../composables/useCounter'
+export default {
+    name:'ClickCounter',
+   // mixins:[counter]
+   setup(props) {
+       const  {incrementCount , incrementCount}= useCounter(100,10)
+       return{
+           count,
+           incrementCount
+       }
+   }
+}
+</script>
+<style lang="">
+    
+</style>
